f50fac1e9487761593fc12d001fc99b66e9bc016
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-17 17:18:16 +0530

    create post route for send email.

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -1,10 +1,45 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from flask_mail import Message
+from app import mail
+from app.models.forms import ContactForm
 
 
 
 contact_bp = Blueprint('contact', __name__)
 
 
-@contact_bp.route('/contact')
+@contact_bp.route('/contact', methods=["GET","POST"])
 def contact():
-    return render_template('contact.html')
+
+    form = ContactForm()
+
+    if form.validate_on_submit():
+
+        sender_name = form.name.data
+        sender_email = form.email.data
+        message_text = form.message.data
+
+        msg = Message(
+            subject="New Portfolio Contact Message",
+            recipients=["YOUR_EMAIL@gmail.com"],
+            body=f"""
+New message from your portfolio.
+
+Sender Email:
+{sender_email}
+
+Sender Email:
+{sender_name}
+
+Message:
+{message_text}
+"""
+        )
+
+        mail.send(msg)
+
+        flash("Message sent successfully!", "success")
+
+        return redirect(url_for("contact.contact"))
+
+    return render_template("contact.html", form=form)
