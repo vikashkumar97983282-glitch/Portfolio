bed7bf46a5754e75b9cd70140f64cf7b1108d243
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-17 17:09:48 +0530

    add email form .

---
+++ app/models/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+
+class ContactForm(FlaskForm):
+
+    name = StringField(
+        "Name",
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    email = StringField(
+        "Email",
+        validators=[
+            DataRequired(),
+            Email()
+        ]
+    )
+
+    message = TextAreaField(
+        "Message",
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    submit = SubmitField("Send Message")
